4e9b5c430f1c05eeabb1aa9d4ca0d59ab8c79e32
hintou <jonas.techdeveloper@gmail.com>
2026-03-12 08:43:18 -0300

    feat: janela inicial funcional

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout)
+import json, sys
+
+class MainWindow(QWidget):
+    def __init__():
+        super().__init__()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.show()
+    sys.exit(app.exec())
